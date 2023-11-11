f50eb10ceb4b5c817ae18097ab5c1f3badf51118
Dang Nguyen <69738431+ddawngnguyeexn@users.noreply.github.com>
2023-11-10 21:59:57 -0800

    update Home page
    some basic information on Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,10 @@ function Home(){
     return(
         <>
             <section>
-                <h1>An API test </h1>
+                <h1>A simple tool to explore the universe of Ice and Fire</h1>
+                <p> If you are a fan of HBO series Game Of Thrones, you will find this tool helpful to find out more details about the Characters and Houses </p>
+                <p> This tool pulls the information from the Ice and Fire API by Joakim Skoog </p>
+
             </section>
         
         </>
